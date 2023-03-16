73d62c5000230dbaea3e4fa2279e26bb45491aad
mrunalshri <mrunalyetekar@gmail.com>
2023-03-16 22:32:00 +0000

    completed activity 3: - added characterFavourites use state hook variable -passed down props to implement favourites toggle - added toggleFavouriteForCharacter method - modified button text to display by conditional rendering

--- src/components/character.tsx
+++ src/components/character.tsx
@@ -2,22 +2,45 @@ import { DisneyCharacter } from "../disney_character";
 
 interface CharacterProps {
   character: DisneyCharacter;
+  characterFavourites: Array<number>;
+  updateFavourites: (favourites: Array<number>) => void;
 }
 
-const Character: React.FC<CharacterProps> = ({ character }) => {
+const Character: React.FC<CharacterProps> = ({
+  character,
+  characterFavourites,
+  updateFavourites,
+}) => {
+  const toggleFavouriteForCharacter = (characterId: number): void => {
+    if (!characterFavourites.includes(characterId)) {
+      // add to favourites
+      updateFavourites([...characterFavourites, characterId]);
+    } else {
+      // remove from favourites
+      const updatedFavourites = characterFavourites.filter(
+        (id) => id !== characterId
+      );
+      updateFavourites(updatedFavourites);
+    }
+  };
   return (
     <article className="card">
-
       <h2>{character.name}</h2>
 
-      <button className="card__button ">Add to favourites</button>
+      <button
+        className="card__button "
+        onClick={() => toggleFavouriteForCharacter(character._id)}
+      >
+        {!characterFavourites.includes(character._id)
+          ? "Add to Favourites"
+          : "Favourited"}
+      </button>
 
       <img
         className="card__img"
         src={character.imageUrl}
         alt={character.name}
       />
-
     </article>
   );
 };
